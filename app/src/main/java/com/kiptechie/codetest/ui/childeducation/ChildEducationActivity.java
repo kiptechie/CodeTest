af468be41b552ba12715525dee9acfaca69bf27a
Timothy K. Serem <kipkorirtimserem@outlook.com>
2020-01-24 16:43:19 +0300

    Initial commit

---
+++ app/src/main/java/com/kiptechie/codetest/ui/childeducation/ChildEducationActivity.java
@@ -0,0 +1,16 @@
+package com.kiptechie.codetest.ui.childeducation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.kiptechie.codetest.R;
+
+public class ChildEducationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child_education);
+    }
+}
